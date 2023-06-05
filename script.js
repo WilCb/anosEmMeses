9b0c32428237fb73832fd0c35f6cbc03c126dac2
WilCb <williams.lima989@gmail.com>
2023-06-05 19:18:43 -0300

    adicionado copyright

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+//pega o botão, input e span do html
 const calcular = document.getElementById('btn');
 const valor = document.getElementById('valor');
 var resultado = document.getElementById('resultado');
-valor.focus()
+
+//foca no input
+valor.focus();
+
+//calcula o valor digitado no input ao clicar no botão
 calcular.addEventListener('click', function() {
     
     var calculo = Number(valor.value) * 12;
     resultado.innerHTML = calculo;
 })
-
-
