b93e048d1f9dcf41b8d1c1f674f3b955540730b4
WilCb <williams.lima989@gmail.com>
2023-06-05 18:03:24 -0300

    ajuste de tela para smartphone

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const numero = document.getElementById('numero');
 var resultado = document.getElementById('resultado');
 numero.focus()
 calcular.addEventListener('click', function() {
-
+    
     var calculo = Number(numero.value) * 12;
     resultado.innerHTML = calculo;
 })
