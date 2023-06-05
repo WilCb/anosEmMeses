d1444d0f5062aa7bd1d7ff7b22e7d8a6aacc3472
WilCb <williams.lima989@gmail.com>
2023-06-05 17:17:43 -0300

    tornando a calculadora de meses funcional

---
+++ script.js
@@ -0,0 +1,11 @@
+const calcular = document.getElementById('btn');
+const numero = document.getElementById('numero');
+var resultado = document.getElementById('resultado');
+numero.focus()
+calcular.addEventListener('click', function() {
+
+    var calculo = Number(numero.value) * 12;
+    resultado.innerHTML = calculo;
+})
+
+
